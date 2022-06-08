3923e8805d6213bc54493b61f56df7126429e95f
Charming0420 <tommy20326999@gmail.com>
2022-06-09 00:57:54 +0800

    0609 12:57

--- NFT Analytics/front end/js/backend1.js
+++ NFT Analytics/front end/js/backend1.js
@@ -1,6 +1,3 @@
-    use_name_find_nft_contract_all("0x081FAC886FDcacdC826C4135ED4BA5b65ecEcFb8") //預設 到時候換掉
-
-
     //----------------------------------------------------------------------------------------------//
     //                            用合約地址找SLUG資料之 API function                                 //
     //---------------------------------------------------------------------------------------------// 
@@ -17,65 +14,52 @@
         .catch((err) => console.error(err))
     }
 
-
+    
 
     // //----------------------------------------------------------------------------------------------//
-    // //                                用SLUG找詳細資料之 API function                                 //
+    // //                                合約找SLUG再找詳細外部資料API function                           //
     // //---------------------------------------------------------------------------------------------//     
+    // async function find_nft_contract_all() {
+    //   var contract_address = document.getElementById("address").value
+    //   const nft_detail_info = await find_slug(contract_address);
+    //   console.log(nft_detail_info);
+    //   ///NAME 
+    //   name=nft_detail_info.name
+    //   name_1 = name.replace(/\s*/g,"");
+    //   console.log(name_1);
+    //   ///SLUG
+    //   nft_slug2 = nft_detail_info.collection.slug 
+    //   ///DISCORD URL
+    //   discord = nft_detail_info.collection.discord_url
+    //   ///TWITTER URL
+    //   if(nft_detail_info.collection.twitter_username==null)
+    //   {
+    //     twitter = 'https://twitter.com/'+name_1
+    //     console.log("No twitter");
+    //   }
+    //   else
+    //   {
+    //     twitter_name = nft_detail_info.collection.twitter_username
+    //     twitter = 'https://twitter.com/'+twitter_name
+    //   }
+    //   ///WEBSITE URL
+    //   website = nft_detail_info.collection.discord_url
+    //   console.log(nft_slug2,discord,twitter,website);
+    //   ///ICON LOGO
+    //   // console.log(nft_detail_info.image_url);
+    //   projectlogo = nft_detail_info.image_url
+    //   console.log("logo : "+projectlogo);
+
+
     //   var nftslugurl =
-    //     "https://api.opensea.io/api/v1/collection/" + nft_slug + "/stats"
+    //     "https://api.opensea.io/api/v1/collection/" + nft_slug2 + "/stats"
     //   const options = { method: "GET", headers: { Accept: "application/json" } }
     //   fetch(nftslugurl, options)
     //     .then(response => response.json())
     //     .then((response) => console.log(response))
     //     .catch((err) => console.error(err))
-    
-
-
-    //----------------------------------------------------------------------------------------------//
-    //                                合約找SLUG再找詳細外部資料API function                           //
-    //---------------------------------------------------------------------------------------------//     
-    async function find_nft_contract_all() {
-      var contract_address = document.getElementById("address").value
-      const nft_detail_info = await find_slug(contract_address);
-      console.log(nft_detail_info);
-      ///NAME 
-      name=nft_detail_info.name
-      name_1 = name.replace(/\s*/g,"");
-      console.log(name_1);
-      ///SLUG
-      nft_slug2 = nft_detail_info.collection.slug 
-      ///DISCORD URL
-      discord = nft_detail_info.collection.discord_url
-      ///TWITTER URL
-      if(nft_detail_info.collection.twitter_username==null)
-      {
-        twitter = 'https://twitter.com/'+name_1
-        console.log("No twitter");
-      }
-      else
-      {
-        twitter_name = nft_detail_info.collection.twitter_username
-        twitter = 'https://twitter.com/'+twitter_name
-      }
-      ///WEBSITE URL
-      website = nft_detail_info.collection.discord_url
-      console.log(nft_slug2,discord,twitter,website);
-      ///ICON LOGO
-      // console.log(nft_detail_info.image_url);
-      projectlogo = nft_detail_info.image_url
-      console.log("logo : "+projectlogo);
-
-
-      var nftslugurl =
-        "https://api.opensea.io/api/v1/collection/" + nft_slug2 + "/stats"
-      const options = { method: "GET", headers: { Accept: "application/json" } }
-      fetch(nftslugurl, options)
-        .then(response => response.json())
-        .then((response) => console.log(response))
-        .catch((err) => console.error(err))
       
-    }
+    // }
 
 
 
@@ -135,9 +119,9 @@
     //----------------------------------------------------------------------------------------------//
     //                                      名字找合約Main function                                  //
     //---------------------------------------------------------------------------------------------//    
-    async function find_nft_contract_by_name()
+    async function find_nft_contract_by_name(nftname)
     {
-      var name_to_contract_address = await search_by_name(1)
+      var name_to_contract_address = await search_by_name(nftname)
       // console.log(name_to_contract_address);
       if (name_to_contract_address.total=='0')
       {
@@ -158,11 +142,11 @@
     //----------------------------------------------------------------------------------------------//
     //                    將nft name名字傳入API找到合約後RETURN回找合約Main function                    //
     //---------------------------------------------------------------------------------------------//    
-    function search_by_name()
+    function search_by_name(nftname)
     {
-      var nft_name = document.getElementById("nft_name").value
+      // var nft_name = document.getElementById("nft_name").value
       var searchapi =
-        "https://deep-index.moralis.io/api/v2/nft/search?chain=eth&format=decimal&q=" + nft_name + "&filter=name"
+        "https://deep-index.moralis.io/api/v2/nft/search?chain=eth&format=decimal&q=" + nftname + "&filter=name"
       const options = { method: "GET", headers: { "Accept": "application/json","X-API-Key": "6FAF5fwmYKqG4MZR7xlf5lXYRVGc4wVqsNMBfhC37fJJjEOQVJLWYEZMqaBZFQv7"} }
       return fetch(searchapi, options)
         .then(response => {return response.json()})
@@ -179,10 +163,11 @@
       var historyfp = await show_history_FP(address);
       if(historyfp.error_code!=null)
       {
-        console.log("No related trading data here!");
+        console.log("X No related trading data here!");
       }
       else
       {
+        console.log(("O find related trading data here!"));
         process_json(historyfp) //去做json前處理
       }
     }
@@ -194,12 +179,8 @@
     //---------------------------------------------------------------------------------------------//
     function show_history_FP(address)
     {
-      // const currentDay =moment().format('YYYY-MM-DD')
-      // let from = moment().subtract(90, 'days').format('YYYY-MM-DD')
       let from = "2022-03-06"
       let currentDay = "2022-04-06"
-      // var address="0x82016d4ad050ef4784e282b82a746d3e01df23bf" //暫時的合約  
-
       var historyfpapi = "https://api.covalenthq.com/v1/1/nft_market/collection/"+address+"/?from="+from+"&to="+currentDay
       return fetch(historyfpapi, {headers:{
         Authorization: "Basic Y2tleV9hZWI0NGQ0YzAwNWU0NTFhOTg4NmVhYTk2OGY6IA=="
@@ -213,18 +194,18 @@
     //---------------------------------------------------------------------------------------------//    
     function process_json(historyfp)
     {
-      // console.log(historyfp);
+      console.log(historyfp);
       var priceXdata =[]
-      for(var i = 0; i < 31; i++)
+      for(var i = 0; i < historyfp.data.items.length; i++)
       {
+        if(historyfp.data.items[i].opening_date)
         var newdate = historyfp.data.items[i].opening_date
         var newprice = historyfp.data.items[i].floor_price_quote_7d;
         newdate=newdate.substr(5)
-        // console.log(newdate);
         add={day:newdate,floorprice:newprice}
         priceXdata.push(add)
       }
-      console.log(JSON.stringify(priceXdata));
+      // console.log(JSON.stringify(priceXdata));
       d3.select(window).on("resize", drawdiagram(priceXdata));
     }
 
@@ -236,7 +217,7 @@
     async function drawdiagram(data)
     {
         const parseTime = d3.timeParse('%m-%d')
-        for(var x = 0; x < 31; x++)
+        for(var x = 0; x < data.length; x++)
         {
             data[x].day = parseTime(data[x].day);
         }
